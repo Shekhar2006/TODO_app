6fbebddd009d81d5659d1b60ccc3dd1af321adb8
Shekhar2006 <shekharphulsare@gmail.com>
2025-09-16 19:35:42 +0530

    Remove node_modules from repo and update .gitignore

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const taskRoutes = require('./routes/taskRoutes');
+dotenv.config();
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+// mongodb connection
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> console.log(`MongoDB Connected!`))
+.catch((err)=> console.log(`MongoDB Connection Error : ${err}`));
